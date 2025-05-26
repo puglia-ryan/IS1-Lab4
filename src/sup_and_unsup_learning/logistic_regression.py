65ba1d7e75809a4ca5e14a198cab3e1a830954dd
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-05-26 12:13:38 +0200

    Implemented constructor and sigmoid methods for the Logistic regression class

--- src/sup_and_unsup_learning/logistic_regression.py
+++ src/sup_and_unsup_learning/logistic_regression.py
@@ -1,11 +1,13 @@
+import math
 # This file implements the logistic regression algorithm (supervised learning)
 
 class LogisticRegression:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, lr=0.1, n_iters=1000) -> None:
+        self.lr = lr
+        self.n_iters = n_iters
 
     def _sigmoid(self, z):
-        pass
+        return 1 / (1 + math.exp(-z))
 
     def fit(self, X, y):
         pass

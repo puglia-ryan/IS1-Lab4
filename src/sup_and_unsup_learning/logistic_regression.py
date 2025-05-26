182525182b511caab45b7aa9fc117c8a450ee76e
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-05-26 12:26:17 +0200

    Fully implemented the logistic regression class along with the necessary methods

--- src/sup_and_unsup_learning/logistic_regression.py
+++ src/sup_and_unsup_learning/logistic_regression.py
@@ -10,7 +10,40 @@ class LogisticRegression:
         return 1 / (1 + math.exp(-z))
 
     def fit(self, X, y):
-        pass
-    
+        n_samples, n_features = len(X), len(X[0])
+        
+        #weights and biases are initialised
+        self.w = [0.0]*n_features
+        self.b = 0-0
+
+        for _ in range(self.n_iters):
+            dw = [0.0]*n_features
+            db = 0.0
+            
+            # Computing the gradients:
+            for xi, yi in zip(X, y):
+                #The linear model
+                z = sum(wi*xx for wi, xx in zip(self.w, xi)) + self.b
+                y_pred = self._sigmoid(z)
+
+                # gradients
+                error = y_pred -yi
+                for j in range(n_features):
+                    dw[j] += xi[j] * error
+                db += error
+            
+            # Average is computed
+            dw = [d / n_samples for d in dw]
+            db /= n_samples
+            
+            # Parameters are updated
+            self.w = [wi - self.lr * dwi for wi, dwi in zip(self.w, dw)]
+            self.b -= self.lr * db
+
     def predict(self, X):
-        pass
+        y_preds = []
+        for xi in X:
+            z = sum(wi*xx for wi, xx in zip(self.w, xi)) + self.b
+            y_pred = self._sigmoid(z)
+            y_preds.append(1 if y_pred >= 0.5 else 0)
+        return y_preds

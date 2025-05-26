7e6ddfd3b80271b4a9480e35651d3bca3dac2ae1
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-05-26 12:10:45 +0200

    Created file to implement logistic regression algorithm and added skeleton class/methods

---
+++ src/sup_and_unsup_learning/logistic_regression.py
@@ -0,0 +1,14 @@
+# This file implements the logistic regression algorithm (supervised learning)
+
+class LogisticRegression:
+    def __init__(self) -> None:
+        pass
+
+    def _sigmoid(self, z):
+        pass
+
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass

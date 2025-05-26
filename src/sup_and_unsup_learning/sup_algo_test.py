2513fa10ef0c650f884d8ff896039cfba2e0f7a2
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-05-26 15:26:26 +0200

    Data preprocessing for the heart failure dataset

--- src/sup_and_unsup_learning/sup_algo_test.py
+++ src/sup_and_unsup_learning/sup_algo_test.py
@@ -8,3 +8,18 @@ from logistic_regression import LogisticRegression
 
 #Load heart failure dataset into pandas dataframe
 df = pd.read_csv("src/sup_and_unsup_learning/heart.csv")
+
+# Seperate features and target
+X = df.drop("DEATH_EVENT", axis=1).values.tolist()
+y = df["DEATH_EVENT"].values.tolist()
+
+# Splitting the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42, stratify=y
+)
+
+# Feature scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(np.array(X_train)).tolist()
+X_test = scaler.fit_transform(np.array(X_test)).tolist()
+
